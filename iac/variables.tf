0e10f59dca99e713216c2a96039c729a6895cfd0
shreyashjoshi <43450174+shreyashjoshi@users.noreply.github.com>
2025-09-26 03:22:34 +0530

    snd

--- iac/variables.tf
+++ iac/variables.tf
@@ -16,15 +16,15 @@ variable "azs" {
   type        = list(string)
 }
 
-variable "public_subnets" {
-  description = "List of public subnet CIDRs"
-  type        = list(string)
-}
+# variable "public_subnets" {
+#   description = "List of public subnet CIDRs"
+#   type        = list(string)
+# }
 
-variable "private_subnets" {
-  description = "List of private subnet CIDRs"
-  type        = list(string)
-}
+# variable "private_subnets" {
+#   description = "List of private subnet CIDRs"
+#   type        = list(string)
+# }
 
 variable "enable_nat_gateway" {
   description = "Enable NAT Gateway"
