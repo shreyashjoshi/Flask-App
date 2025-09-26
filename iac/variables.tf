f03eafb1c5b84a913108950a75d9c59846728721
shreyashjoshi <43450174+shreyashjoshi@users.noreply.github.com>
2025-09-26 06:28:34 +0530

    commit

--- iac/variables.tf
+++ iac/variables.tf
@@ -6,6 +6,12 @@ variable "cluster_name" {
   default     = "cluster-1"
 }
 
+variable "vpc_name" {
+  description = "Name of the VPC"
+  type        = string
+  default     = "my-vpc"
+}
+
 variable "cidr_block" {
   description = "CIDR block for the VPC"
   type        = string
